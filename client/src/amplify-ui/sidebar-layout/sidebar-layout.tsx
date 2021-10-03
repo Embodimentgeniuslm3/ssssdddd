835a74a0d6800a4f250e09804714a45ea247cdea
superb-firefly-322618@appspot.gserviceaccount.com <78320452+imgod123456@users.noreply.github.com>
2021-10-03 01:10:50 -0700

    Initialize run_script

---
+++ client/src/amplify-ui/sidebar-layout/sidebar-layout.tsx
@@ -0,0 +1,35 @@
+import {Component, Host, State, h} from "@stencil/core";
+import {MQTablet} from "../styles/media";
+import {sidebarLayoutContext} from "./sidebar-layout.context";
+import {sidebarLayoutStyle} from "./sidebar-layout.style";
+import {ToggleInView} from "./sidebar-layout.types";
+
+@Component({tag: "amplify-sidebar-layout", shadow: false})
+export class AmplifySidebarLayout {
+  // Slice off the "@media " string at the start for use in JS instead of CSS
+  readonly MQTabletJS = MQTablet.substring(6);
+  // If the media query matches, then the user is on desktop and should see the menu by default
+  @State() sidebarInView = window.matchMedia(this.MQTabletJS).matches;
+
+  toggleSidebarInView: ToggleInView = () =>
+    (this.sidebarInView = !this.sidebarInView);
+
+  render() {
+    return (
+      <Host>
+        <div class={sidebarLayoutStyle}>
+          <sidebarLayoutContext.Provider
+            state={{
+              inView: this.sidebarInView,
+              toggleInView: this.toggleSidebarInView,
+            }}
+          >
+            <slot name="sidebar" />
+            <slot name="main" />
+            <slot name="toc" />
+          </sidebarLayoutContext.Provider>
+        </div>
+      </Host>
+    );
+  }
+}
